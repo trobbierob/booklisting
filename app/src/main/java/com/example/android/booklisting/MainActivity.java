bc1d2eda0dacaa33b0685876b2709938f1fa7e95
trobbierob <troberts@my.hpu.edu>
2017-01-15 04:00:26 -0500

    Basic layout, and Search button have been created.

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.android.booklisting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mSearchEditText;
+    private TextView mResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchEditText = (EditText) findViewById(R.id.editText_query);
+        mResultsTextView = (TextView) findViewById(R.id.search_results);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemSelected = item.getItemId();
+        if (menuItemSelected == R.id.action_bar_search) {
+            Toast.makeText(getBaseContext(), "Search", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/android/booklisting/Network.java
@@ -0,0 +1,8 @@
+package com.example.android.booklisting;
+
+/**
+ * Created by Sogekingu on 1/15/17.
+ */
+
+public class Network {
+}
